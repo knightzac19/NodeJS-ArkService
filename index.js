30e1bb35e79e28476008b9f2a0870d111d4cc8a4
Zachary Knight <zknight@practicalit.info>
2016-07-16 21:36:12 -0400

    added some verification checks and settings.json initialization

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 /*jshint esversion: 6 */
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.cached.Database('./players.sqlite');
-var arkdata = require('arkdata');
-var player = arkdata.player;
-var tribe = arkdata.tribe;
 var fs = require('fs');
-var playerModel = require("./lib/playermodel.js");
-var tribeModel = require("./lib/tribemodel.js");
-var authModel = require("./lib/authmodel.js");
-var arkserver = require("./lib/server.js");
-
-
 var https = require('https');
 var express = require('express');
 var app = express();
@@ -19,17 +10,8 @@ var options = {
     // cert: fs.readFileSync('server.crt')
 };
 var bodyParser = require('body-parser');
-
-
+var path = require('path');
 const crypto = require('crypto');
-
-const settings = JSON.parse(fs.readFileSync('./settings.json', 'utf8'));
-if (settings.daemon_mode === true) {
-    require('daemon')();
-}
-const api_settings = settings.api_settings;
-const server_settings = settings.server_config;
-
 
 function checkHash(key, res, cb) {
     //.update('sdblas%ew5@trast')
@@ -47,20 +29,104 @@ function checkHash(key, res, cb) {
     });
     // return false;
 }
+var defaultSettings = {
+    "daemon_mode": false,
+    "sourcequery": {
+        "host": "<CHANGEME>",
+        "port": 27015,
+        "rconport": 27020,
+        "rconpass": "<changeme>"
+    },
+    "server_config": {
+        "host": "localhost",
+        "port": 8081,
+        "use_ajax": true,
+        "allowed_server": "http://localhost",
+        "ark_path": "C:\\Steam\\steamapps\\common\\ARK",
+        "steam_key": "YOURSTEAMKEY",
+        "cache_refresh": 1800000
+    },
+    "api_settings": {
+        "secret": "<CHANGEME>",
+        "hash_type": "sha256"
+    }
+};
+var settings;
+try {
+    settings = JSON.parse(fs.readFileSync('./settings.json', 'utf8'));
+    if (JSON.stringify(settings) == JSON.stringify(defaultSettings)) {
+        console.error("ERROR: Failed to start, default settings detected!");
+        process.exit(1);
+    }
+} catch (err) {
+    settings = defaultSettings;
+    fs.writeFileSync('./settings.json', JSON.stringify(settings, null, 4));
+    console.error("Your settings.json has been initialized, please edit it before running again!");
+    process.exit(1);
+}
 
+if(Object.keys(defaultSettings).length == Object.keys(settings).length && Object.keys(defaultSettings.sourcequery).length == Object.keys(settings.sourcequery).length && Object.keys(defaultSettings.server_config).length == Object.keys(settings.server_config).length && Object.keys(defaultSettings.api_settings).length == Object.keys(settings.api_settings).length) {
+	console.log("Config verification finished!");
+} else {
+	console.error("ERROR: Config verification failed. Please ensure all options are there.");
+	process.exit(1);
+}
+Object.freeze(settings);
+try {
+    var ark = fs.readFileSync(path.join(settings.server_config.ark_path, "version.txt"));
+} catch (err) {
+    console.error("ERROR: Ark not found...Exiting!");
+    process.exit(1);
+}
+
+
+if (settings.daemon_mode === true) {
+    require('daemon')();
+}
+const api_settings = settings.api_settings;
+const server_settings = settings.server_config;
+
+if (!settings)
+    defaultSettings.forEach(elem => {
+
+    });
+
+
+var arkdata = require('arkdata');
+var player = arkdata.player;
+var tribe = arkdata.tribe;
+var playerModel = require("./lib/playermodel.js");
+var tribeModel = require("./lib/tribemodel.js");
+var authModel = require("./lib/authmodel.js");
+var arkserver = require("./lib/server.js");
 
 player.setupPlayers(function() {
     tribe.setupTribes(function() {
-        setTimeout(function() {
-            //default is 30 minutes to refresh
-            setInterval(function() {
-                console.log("Refreshing Cache");
-                player.setupPlayers(function() {
-                    tribe.setupTribes(function() {
-                        console.log("Player/Tribe Data refreshed!");
-                    });
+        if (!server_settings.cache_refresh) {
+            settings.server_config.cache_refresh = 1800000;
+            fs.writeFileSync('./settings.json', JSON.stringify(settings, null, 4));
+        }
+        if (!server_settings.host) {
+            settings.server_config.host = null;
+            fs.writeFileSync('./settings.json', JSON.stringify(settings, null, 4));
+        }
+        if (!server_settings.port) {
+            settings.server_config.port = 8081;
+            fs.writeFileSync('./settings.json', JSON.stringify(settings, null, 4));
+        }
+        if (!server_settings.use_ajax) {
+            settings.server_config.use_ajax = true;
+            fs.writeFileSync('./settings.json', JSON.stringify(settings, null, 4));
+        }
+
+        //default is 30 minutes to refresh
+        setInterval(function() {
+            console.log("Refreshing Cache");
+            player.setupPlayers(function() {
+                tribe.setupTribes(function() {
+                    console.log("Player/Tribe Data refreshed!");
                 });
-            }, server_settings.cache_refresh);
+            });
         }, server_settings.cache_refresh);
 
 
